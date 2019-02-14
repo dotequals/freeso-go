4607311c6a2d0f26bf7961122d29985a884b827f
dotequals <strdotequals@gmail.com>
2019-02-13 20:59:46 -0800

    Redux storage for directories and permissions

--- src/redux/indexReducer.js
+++ src/redux/indexReducer.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux';
 
 import blog from './blog';
 import forum from './forum';
+import installed from './installed';
 import remesh from './remesh';
 import settings from './settings';
 import system from './system';
@@ -9,9 +10,10 @@ import system from './system';
 const indexReducer = combineReducers({
   blog,
   forum,
+  installed,
   remesh,
   settings,
   system,
 });
 
-export default indexReducer
+export default indexReducer;
---
+++ src/redux/installed.js
@@ -0,0 +1,92 @@
+export const TOGGLE_GO_PERMS ='TOGGLE_GO_PERMS';
+export const SET_GO_DIR = 'SET_GO_DIR';
+export const TOGGLE_TSO_PERMS = 'TOGGLE_TSO_PERMS';
+export const SET_TSO_DIR = 'SET_TSO_DIR';
+export const TOGGLE_FSO_PERMS = 'TOGGLE_FSO_PERMS';
+export const SET_FSO_DIR = 'SET_FSO_DIR';
+export const TOGGLE_TS1_PERMS = 'TOGGLE_TS1_PERMS';
+export const SET_TS1_DIR = 'SET_TS1_DIR';
+export const TOGGLE_ST_PERMS = 'TOGGLE_ST_PERMS';
+export const SET_ST_DIR = 'SET_ST_DIR';
+
+const initialState = {
+  goDir: '',
+  goPerms: false,
+  tsoDir: '',
+  tsoPerms: false,
+  fsoDir: '',
+  fsoPerms: false,
+  ts1Dir: '',
+  ts1Perms: false,
+  stDir: '',
+  // Needs to be installed locally, so always true
+  stPerms: true,
+}
+
+export default function reducer(state = initialState, action = {}) {
+  switch (action.type) {
+    case TOGGLE_GO_PERMS:
+      return {
+        ...state,
+        goPerms: action.data,
+      };
+    case SET_GO_DIR:
+      return {
+        ...state,
+        goDir: action.data,
+      };
+    case TOGGLE_TSO_PERMS:
+      return {
+        ...state,
+        tsoPerms: action.data,
+      };
+    case SET_TSO_DIR:
+      return {
+        ...state,
+        tsoDir: action.data,
+      };
+    case TOGGLE_FSO_PERMS:
+      return {
+        ...state,
+        fsoPerms: action.data,
+      };
+    case SET_FSO_DIR:
+      return {
+        ...state,
+        fsoDir: action.data,
+      };
+    case TOGGLE_TS1_PERMS:
+      return {
+        ...state,
+        ts1Dir: action.data,
+      };
+    case SET_TS1_DIR:
+      return {
+        ...state,
+        ts1Dir: action.data,
+      };
+    case TOGGLE_ST_PERMS:
+      return {
+        ...state,
+        stPerms: action.data,
+      };
+    case SET_ST_DIR:
+      return {
+        ...state,
+        stDir: action.data,
+      };
+    default:
+      return state;
+  }
+}
+
+export const toggleGoPerms = data => ({ type: TOGGLE_GO_PERMS, data });
+export const setGoDir = data => ({ type: SET_GO_DIR, data });
+export const toggleTsoPerms = data => ({ type: TOGGLE_TSO_PERMS, data });
+export const setTsoDir = data => ({ type: SET_TSO_DIR, data });
+export const toggleFsoPerms = data => ({ type: TOGGLE_FSO_PERMS, data });
+export const setFsoDir = data => ({ type: SET_FSO_DIR, data });
+export const toggleTs1Perms = data => ({ type: TOGGLE_TS1_PERMS, data });
+export const setTs1Dir = data => ({ type: SET_TS1_DIR, data });
+export const toggleStPerms = data => ({ type: TOGGLE_ST_PERMS, data });
+export const setStDir = data => ({ type: SET_ST_DIR, data });
