eb66ad8c9026682961f5d9b0755af1523306b656
dotequals <strdotequals@gmail.com>
2019-04-02 13:21:52 -0700

    Prettier permission warnings

--- src/Permissions/index.js
+++ src/Permissions/index.js
@@ -1,18 +1,15 @@
 import React from 'react';
 
+import styles from './index.module.css';
+
 const Permissions = props => {
   const { fsoDir, fsoPerms, goPerms, stDir, stPerms, ts1Dir, ts1Perms, tsoDir, tsoPerms } = props;
-  const goWarning = !goPerms ? (
-    <div className="highlight emphasis">
-      Note: FreeSO Go is currently in a restricted directory it cannot write to. Please move the folder elsewhere on your computer or run the program as Administrator.
-    </div>
-  ) : '';
-  const gameWarnings = (tsoDir && !tsoPerms) || (fsoDir && !fsoPerms) || (ts1Dir && !ts1Perms) || (tsoDir && !tsoPerms) ? `Note: The following directories need need to be made local or ran as Administrator before they will show up in the sidebar: ${tsoDir && !tsoPerms ? 'The Sims Online, ' : ''}${fsoDir && !fsoPerms ? 'FreeSO, ' : ''}${ts1Dir && !ts1Perms ? 'The Sims 1, ': ''}${stDir && !stPerms ? 'Simitone, ' : ''}`.trim().replace(/.$/, '.') : '';
+  const goWarning = !goPerms ? 'Note: FreeSO Go is currently in a restricted directory it cannot write to. Please move the folder elsewhere on your computer or run the program as Administrator.' : '';
+  const gameWarnings = (tsoDir && !tsoPerms) || (fsoDir && !fsoPerms) || (ts1Dir && !ts1Perms) || (tsoDir && !tsoPerms) ? `Note: Please run FreeSO Go as Administrator or click "Make Local" on the Installers tab to enable the following: ${tsoDir && !tsoPerms ? 'The Sims Online, ' : ''}${fsoDir && !fsoPerms ? 'FreeSO, ' : ''}${ts1Dir && !ts1Perms ? 'The Sims 1, ': ''}${stDir && !stPerms ? 'Simitone, ' : ''}`.trim().replace(/.$/, '.') : '';
   return (
     <div>
-      {goWarning}
-      <div className="highlight">
-        {gameWarnings}
+      <div className={styles.default}>
+        {goWarning || gameWarnings}
       </div>
     </div>
   );
